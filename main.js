db5e5ef2abaebf0d5dec01b8778d5cc08cd3be51
tonrea333 <readushartoniii@Readuss-MacBook-Pro.local>
2021-08-01 11:21:23 -0400

    all

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const tableRow = document.createElement("tr");
 const tableHeader = document.createElement("th");
 const tableData = document.createElement("td");
 const tabledataDiv = document.querySelector("#data");
-
+//const topstoriesArray = [];
 
 
 
@@ -22,14 +22,44 @@ function onClick() {
     tableRow.appendChild(tableHeader);
     tableHeader.append("by a great person");
 
+    //Firebase fetch for topstory IDs.
+    fetch("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
+        .then(function (bootdata) {
+            return bootdata.json();
 
-    // fetch("https://hacker-news.firebaseio.com/v0/")
-    //.then(function(bootdata){
-    //  return bootdata.json();
+        })
+        .then(function (data) {
+            console.log(data[1], "data)")
+            //topstoriesArray.push(data)
+            console.log(topstoriesArray, "story")
 
-    //})
-    //.then(function(data) {
-    //  console.log(data, "data)")
-    // })
-
+            fetch( "https://hacker-news.firebaseio.com/v0/item/"+data[1]+".json?print=pretty")
+            .then(function (bootdata){
+                return bootdata.json();
+            })
+            .then(function (data){
+                console.log(data, "data2")
+            })
+        })
+       
+       
+       
+       
+        //for(let i = 0; i <data.length; i++) {
+           // console.log(data.length[i])
+       // }
 }
+
+
+
+
+//printArray()
+    //function printArray(topstoriesArray) {
+        //for (let i = 0; i < topstoriesArray.length; i++) {
+          //  if (topstoriesArray[i] instanceof Array) {
+             //   printArray(topstoriesArray[i]);
+           // } else {
+            //    console.log(topstoriesArray[i]);
+            //}
+        //}
+   // } 
