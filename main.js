e62c85902394e7dcf2bf1d40fd51fed2c9554f41
tonrea333 <readushartoniii@Readuss-MacBook-Pro.local>
2021-08-01 08:45:17 -0400

    all

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const buttonClick = document.querySelector("#button");
 const tableRow = document.createElement("tr");
 const tableHeader = document.createElement("th");
 const tableData = document.createElement("td");
-const tabledataDiv= document.querySelector("#data");
+const tabledataDiv = document.querySelector("#data");
 
 
 
@@ -13,17 +13,23 @@ const tabledataDiv= document.querySelector("#data");
 
 buttonClick.addEventListener("click", onClick);
 
-function onClick(){
+function onClick() {
 
-tableHeader.appendChild(tableData)
-    tabledataDiv.appendChild(tableRow)
-    fetch("https://hacker-news.firebaseio.com/v0/")
-    .then(function(bootdata){
-        return bootdata.json();
 
-    })
-    .then(function(data) {
-        console.log(data, "data)")
-    })
-    
+    tabledataDiv.appendChild(tableRow);
+    tableRow.appendChild(tableHeader);
+    tableHeader.append("A great story");
+    tableRow.appendChild(tableHeader);
+    tableHeader.append("by a great person");
+
+
+    // fetch("https://hacker-news.firebaseio.com/v0/")
+    //.then(function(bootdata){
+    //  return bootdata.json();
+
+    //})
+    //.then(function(data) {
+    //  console.log(data, "data)")
+    // })
+
 }
