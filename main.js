421dc5fe4b54ad5e8530b2af718fd9e55120bfc9
tonrea333 <readushartoniii@Readuss-MacBook-Pro.local>
2021-08-02 10:42:18 -0400

    all

--- main.js
+++ main.js
@@ -14,21 +14,14 @@ function onClick() {
 
 
 
-    const tableRow = document.createElement("tr");
-    tableRow.setAttribute("class", "row");
-    const tableHeader = document.createElement("th");
-    tableHeader.setAttribute("class", "header");
-    const tableData = document.createElement("td");
-    tableData.setAttribute("class", "adata");
-    const tabledataDiv = document.querySelector("#data")
+    
 
 
 
 
 
 
-for(let i = 0; i < data.length; i++){
-    data[i] = data[i] + 1;
+
 
 
 
@@ -45,62 +38,80 @@ for(let i = 0; i < data.length; i++){
 
         .then(function (data) {
             console.log(data[0], "data)")
-
-
-           
-            //let i =0 
-
-            //new_arr = data.map(i => i +1)
-            //console.log(new_arr)
-            
-
-            fetch("https://hacker-news.firebaseio.com/v0/item/" + data[i] + ".json?print=pretty")
-                .then(function (bootdata) {
-                    return bootdata.json();
-                    
-                })
-                .then(function (data) {
-                    console.log(data, "data2")
-                        //console.log(i)
-                    
-
-                    
-
-                    tabledataDiv.appendChild(tableRow);
-                    tableRow.appendChild(tableHeader);
-                    tableHeader.append();
-                    tableRow.appendChild(tableData);
-                    tableData.append();
-                    tabledataDiv.appendChild(tableRow);
-                    const title = data.title;
-                    const url = data.url;
-                    const by = data.by;
+            let i = 0
 
 
 
 
 
 
-                    //setInterval(function () {
-                    //tableHeader.innerHTML = data[i++];
-                    //if (i == 100) {
-                    console.log("top 100")
-                    // clearInterval()
-                    //}
-                    //}, 1000);
+            //function articleGen() {
+            // setInterval(function () {
+            // for(let i = 0; i < data.length; i++){
+            // data[i] = data[i] + 1;
+            // }}, 1000)
 
 
-                    let a = title.link(url);
-                    console.log(tableHeader.innerText);
-                    tableHeader.innerHTML = a;
-                    tableData.innerHTML = " by " + by;
-                    
-                })
+            for (let i = 0; i < data.length; i++) {
+                data[i] = data[i] + 1;
+               
+
+
+
+                //let i =0 
+
+                //new_arr = data.map(i => i +1)
+                //console.log(new_arr)
+
+
+                fetch("https://hacker-news.firebaseio.com/v0/item/" + data[i] + ".json?print=pretty")
+                    .then(function (bootdata) {
+                        return bootdata.json();
+
+                    })
+                    .then(function (data) {
+                        console.log(data, "data2")
+                        console.log(i, "i")
+                        const tableRow = document.createElement("tr");
+                        tableRow.setAttribute("class", "row");
+                        const tableHeader = document.createElement("th");
+                        tableHeader.setAttribute("class", "header");
+                        const tableData = document.createElement("td");
+                        tableData.setAttribute("class", "adata");
+                        const tabledataDiv = document.querySelector("#data")
+
+                        tabledataDiv.appendChild(tableRow);
+                        tableRow.appendChild(tableHeader);
+                        tableHeader.append();
+                        tableRow.appendChild(tableData);
+                        tableData.append();
+                        tabledataDiv.appendChild(tableRow);
+                        const title = data.title;
+                        const url = data.url;
+                        const by = data.by;
+
+
+                        //setInterval(function () {
+                        //tableHeader.innerHTML = data[i++];
+                        //if (i == 100) {
+                        console.log("top 100")
+                        // clearInterval()
+                        //}
+                        //}, 1000);
+
+
+                        let a = title.link(url);
+                        console.log(tableHeader.innerText);
+                        tableHeader.innerHTML = a;
+                        tableData.innerHTML = " by " + by;
+
+                    })
+                // } articleGen()
+                
+            }
 
         })
 
-
-    }
 
 
 }
