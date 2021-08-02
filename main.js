2d4e2399cf72a3c27a99d918e8bee4e5a8328b1c
tonrea333 <readushartoniii@Readuss-MacBook-Pro.local>
2021-08-02 12:26:21 -0400

    all

--- main.js
+++ main.js
@@ -1,31 +1,16 @@
 //testing the hack news api
-const hacknewApi = document.querySelector("#test");
+//const hacknewApi = document.querySelector("#test");
 const buttonClick = document.querySelector("#button");
 ;
 //const topstoriesArray = [];
 
 
 
-
+//Button to action listing 100 top stories
 
 buttonClick.addEventListener("click", onClick);
 
 function onClick() {
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
 
 
     //Firebase fetch for topstory IDs.
@@ -35,14 +20,9 @@ function onClick() {
 
         })
 
-
         .then(function (data) {
-            console.log(data[0], "data)")
+           
             let i = 0
-
-
-
-
 
 
             //function articleGen() {
@@ -50,65 +30,75 @@ function onClick() {
             // for(let i = 0; i < data.length; i++){
             // data[i] = data[i] + 1;
             // }}, 1000)
+            //let y = 0
+            //function x() {
+            //    y = y + 1
+
+                for (let i = 0; i < 50; i++) {
+                    data[i] = data[i] + 1;
+
+                    //let i =0 
+
+                    //new_arr = data.map(i => i +1)
+                    //console.log(new_arr)
+
+                   
+                    fetch("https://hacker-news.firebaseio.com/v0/item/" + data[i] + ".json?print=pretty")
+                        .then(function (bootdata) {
+                            return bootdata.json();
+
+                        })
+                        .then(function (data) {
+                            console.log(data, "data2")
+                            console.log(i, "i")
+                            const tableRow = document.createElement("tr");
+                            tableRow.setAttribute("class", "row");
+                            const tableHeader = document.createElement("th");
+                            tableHeader.setAttribute("class", "header");
+                            const tableData = document.createElement("td");
+                            tableData.setAttribute("class", "adata");
+                            const tableData2 = document.createElement("td");
+                            tableData2.setAttribute("class", "adata");
+                            const tabledataDiv = document.querySelector("#data")
+
+                            tabledataDiv.appendChild(tableRow);
+                            tableRow.appendChild(tableHeader);
+                            tableHeader.append();
+                            tableRow.appendChild(tableData);
+                            tableData.append();
+                            tabledataDiv.appendChild(tableRow);
+                            tableRow.appendChild(tableData);
+                            tableData.append();
+                            tableRow.appendChild(tableData2);
+                            tableData2.append();
+
+                            const title = data.title;
+                            const url = data.url;
+                            const by = data.by;
+                            const score = data.score;
+console.log(title)
+console.log(url)
+console.log(by)
+
+                            //setInterval(function () {
+                            //tableHeader.innerHTML = data[i++];
+                            //if (i == 100) {
+                           
+                            // clearInterval()
+                            //}
+                            //}, 1000);
 
 
-            for (let i = 0; i < data.length; i++) {
-                data[i] = data[i] + 1;
-               
+                            let a = title.link(url);
+                            console.log(tableHeader.innerText);
+                            tableHeader.innerHTML = a;
+                            tableData.innerHTML = " by " + by;
+                            tableData2.innerHTML = "Score "+" "+ score;
 
-
-
-                //let i =0 
-
-                //new_arr = data.map(i => i +1)
-                //console.log(new_arr)
-
-
-                fetch("https://hacker-news.firebaseio.com/v0/item/" + data[i] + ".json?print=pretty")
-                    .then(function (bootdata) {
-                        return bootdata.json();
-
-                    })
-                    .then(function (data) {
-                        console.log(data, "data2")
-                        console.log(i, "i")
-                        const tableRow = document.createElement("tr");
-                        tableRow.setAttribute("class", "row");
-                        const tableHeader = document.createElement("th");
-                        tableHeader.setAttribute("class", "header");
-                        const tableData = document.createElement("td");
-                        tableData.setAttribute("class", "adata");
-                        const tabledataDiv = document.querySelector("#data")
-
-                        tabledataDiv.appendChild(tableRow);
-                        tableRow.appendChild(tableHeader);
-                        tableHeader.append();
-                        tableRow.appendChild(tableData);
-                        tableData.append();
-                        tabledataDiv.appendChild(tableRow);
-                        const title = data.title;
-                        const url = data.url;
-                        const by = data.by;
-
-
-                        //setInterval(function () {
-                        //tableHeader.innerHTML = data[i++];
-                        //if (i == 100) {
-                        console.log("top 100")
-                        // clearInterval()
-                        //}
-                        //}, 1000);
-
-
-                        let a = title.link(url);
-                        console.log(tableHeader.innerText);
-                        tableHeader.innerHTML = a;
-                        tableData.innerHTML = " by " + by;
-
-                    })
-                // } articleGen()
-                
-            }
+                        })
+                    // } articleGen()
+                }
+            //} setInterval(x, 1000);
 
         })
 
