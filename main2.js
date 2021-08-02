85c1303681cf4db54cd58743306d78bcc144db3f
tonrea333 <readushartoniii@Readuss-MacBook-Pro.local>
2021-08-02 15:58:06 -0400

    all

--- main2.js
+++ main2.js
@@ -3,9 +3,20 @@
 //Const needed for page
 
 const buttonClickTwo = document.querySelector("#button2");
-
-
+const hackButtonNews = document.querySelector("#hacklogo");
+//Event listner for ask news
 buttonClickTwo.addEventListener("click", askClick);
+
+//Event listner to return to main page
+hackButtonNews.addEventListener("click", hackClick)
+
+//Function redirects user back to main page
+
+function hackClick(){
+    location.href = "index.html"
+}
+
+
 
 function askClick() {
     fetch("https://hacker-news.firebaseio.com/v0/askstories.json?print=pretty")
