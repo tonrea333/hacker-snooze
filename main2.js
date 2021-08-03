69eaf239f63d348f076b74121eb6936b801377d7
tonrea333 <readushartoniii@Readuss-MacBook-Pro.local>
2021-08-02 22:09:22 -0400

    all

--- main2.js
+++ main2.js
@@ -12,7 +12,7 @@ hackButtonNews.addEventListener("click", hackClick)
 
 //Function redirects user back to main page
 
-function hackClick(){
+function hackClick() {
     location.href = "index.html"
 }
 
@@ -24,7 +24,71 @@ function askClick() {
             return bootdata.json();
         })
         .then(function (data) {
-console.log(data)
+            console.log(data)
+
+            let i = 0
+            for (let i = 0; i < data.length; i++) {
+                data[i] = data[i] + 1;
+
+                fetch("https://hacker-news.firebaseio.com/v0/item/" + data[i] + ".json?print=pretty")
+                    .then(function (bootdata) {
+                        return bootdata.json();
+
+                    })
+                    .then(function (data) {
+                        console.log(data, "data2")
+                        console.log(i, "i")
+                        const tableRow = document.createElement("tr");
+                        tableRow.setAttribute("class", "row");
+                        const tableHeader = document.createElement("th");
+                        tableHeader.setAttribute("class", "header");
+                        const tableData = document.createElement("td");
+                        tableData.setAttribute("class", "adata");
+                        const tableData2 = document.createElement("td");
+                        tableData2.setAttribute("class", "adata");
+                        const tabledataDiv = document.querySelector("#data2")
+
+                        tabledataDiv.appendChild(tableRow);
+                        tableRow.appendChild(tableHeader);
+                        tableHeader.append();
+                        tableRow.appendChild(tableData);
+                        tableData.append();
+                        tabledataDiv.appendChild(tableRow);
+                        tableRow.appendChild(tableData);
+                        tableData.append();
+                        tableRow.appendChild(tableData2);
+                        tableData2.append();
+
+                        const title = data.title;
+                        const text = data.text;
+                        const by = data.by;
+                        //const score = data.score;
+                        console.log(title)
+                        //console.log(url)
+                        console.log(by)
+
+                        //setInterval(function () {
+                        //tableHeader.innerHTML = data[i++];
+                        //if (i == 100) {
+
+                        // clearInterval()
+                        //}
+                        //}, 1000);
+
+
+                        //let a = title.link(url);
+                        console.log(tableHeader.innerText);
+                        tableHeader.innerHTML = title;
+                        tableData.innerHTML = " by " + by;
+                        //tableData2.innerHTML = "Score: " + " " + score;
+
+                    })
+            }
+
+
+
+
+
         })
 
 }
